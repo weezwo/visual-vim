161a4dc683af8f2d4257cfdb1869de8e5e38dce5
Luisa Scavo <luisascavo@gmail.com>
2022-09-14 14:30:40 -0400

    initial commit

---
+++ script/index.js
@@ -0,0 +1,36 @@
+
+// const keyWidth = vh(10);
+// const keyHeight = vh(12);
+
+// let xOffset = vh(3);
+// let yOffset = vh(3);
+
+// const letterOffsetX = vh(1);
+// const letterOffsetY = vh(4);
+
+// const spacer = vh(1);
+
+// const c = document.getElementById('keyboard');
+// const ctx = c.getContext('2d');
+
+// // Q
+// ctx.beginPath();
+// ctx.rect(xOffset, yOffset, keyWidth, keyHeight);
+// ctx.lineWidth = '2';
+// ctx.stroke();
+
+// ctx.font = `${vh(3)}px Arial`
+// ctx.strokeText('Q', xOffset + letterOffsetX, yOffset + letterOffsetY);
+
+// // W
+// ctx.beginPath();
+// ctx.rect(1.5 * xOffset + keyWidth, yOffset, keyWidth, keyHeight);
+// ctx.lineWidth = '2';
+// ctx.stroke();
+
+// ctx.font = `${vh(3)}px Arial`
+// ctx.strokeText('W', xOffset + keyWidth + letterOffsetX * 3, yOffset + letterOffsetY);
+
+// function vh(p) {
+//   return (window.innerHeight / 100) * p;
+// }
